6ea1b4f9e0224b5a536ab1379c908c02cb6d48d2
krm97 <krm97@uclive.ac.nz>
2022-05-27 12:23:12 +1200

    Bids table

--- src/Auction.tsx
+++ src/Auction.tsx
@@ -1,18 +1,36 @@
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import React from "react";
 import axios from "axios";
 import Navbar from "./Navbar";
-import {Box, Container, Divider, Grid, List, Paper} from "@mui/material";
+import {
+    Avatar,
+    Box,
+    Container,
+    Divider,
+    Grid,
+    List,
+    Paper,
+    Table, TableBody,
+    TableCell,
+    TableContainer, TableHead, TableRow,
+    Typography
+} from "@mui/material";
 import auctions from "./Auctions";
 
 const Auction = () => {
     const {id} = useParams();
     const [auction, setAuction] = React.useState({title: 'Not Found', categoryId: -1, sellerId: -1, reserve: -1, endDate: '', auctionId: -1,
         sellerFirstName: '', sellerLastName: '', highestBid: -1, numBids: -1, description: ''})
+    const [bids, setBids] = React.useState<Array<bid>>([{bidderId: -1,
+        amount: 0,
+        firstName: '',
+        lastName: '',
+        timestamp: ''}])
     const [errorFlag, setErrorFlag] = React.useState(false)
     const [errorMessage, setErrorMessage] = React.useState("")
     React.useEffect(() => {
         getAuction()
+        getBids()
     }, [id])
     const styles = {
         auctionPhoto: {
@@ -24,7 +42,28 @@ const Auction = () => {
             justifyContent: 'center',
             border: '1px solid black',
             backgroundColor: 'lightgray'
-        }
+        },
+        reserveNotMet: {
+            color: 'gray',
+        },
+        reserveMet: {
+            color: 'orange'
+        },
+        center: {
+            display: 'flex',
+            alignItems: 'center'
+        },
+    }
+    const getBids = () => {
+        axios.get('http://localhost:4941/api/v1/auctions/' + id + '/bids')
+            .then(async (response) => {
+                setErrorFlag(false)
+                setErrorMessage("")
+                setBids(response.data)
+            }, (error) => {
+                setErrorFlag(true)
+                setErrorMessage(error.toString())
+            })
     }
     const getAuction = () => {
         axios.get('http://localhost:4941/api/v1/auctions/' + id)
@@ -36,6 +75,110 @@ const Auction = () => {
                 setErrorFlag(true)
                 setErrorMessage(error.toString())
             })
+    }
+    const auctionReserve = () => {
+        if (!auction.highestBid) {
+            return (
+                <Grid container spacing={1} style={{justifyContent: "center"}}>
+                    <Grid item sm={12} style={styles.center}>
+                <h3 style={styles.reserveNotMet}>No bids</h3>
+                    </Grid>
+                <h3 >Reserve: ${auction.reserve}</h3>
+                </Grid>
+            )
+        }
+        if (auction.reserve > auction.highestBid) {
+            return (
+                <Grid container spacing={1} style={{justifyContent: "center"}}>
+                    <Grid item sm={2.5} style={styles.center} sx={{justifyContent: 'center'}}>
+                        <Typography variant="h6" style={styles.reserveNotMet}>Current Bid: ${auction.highestBid} </Typography>
+                    </Grid>
+                    <Grid item sm={0.5} style={styles.center}>
+                        <Avatar sx={{height: 50, width: 50}} alt={bids[0].firstName + " " + bids[0].lastName} src={"http://localhost:4941/api/v1/users/" + bids[0].bidderId + "/image"}/>
+                    </Grid>
+                    <Grid item sm={2.5} style={styles.center} sx={{justifyContent: 'center'}}>
+                        <Typography variant="h6"><Link to={"users/" + bids[0].bidderId}>{bids[0].firstName} {bids[0].lastName}</Link></Typography>
+                    </Grid>
+                    <Grid item sm={12} style={{textAlign: 'center'}}>
+                        <Typography variant="h6">Reserve: ${auction.reserve}</Typography>
+                    </Grid>
+                </Grid>
+            )
+        } else {
+            return (
+                <Grid container spacing={1} style={{justifyContent: "center"}}>
+                    <Grid item sm={2.5} style={styles.center} sx={{justifyContent: 'center'}}>
+                        <Typography variant="h6" style={styles.reserveMet}>Current Bid: ${auction.highestBid} </Typography>
+                    </Grid>
+                    <Grid item sm={0.5} style={styles.center}>
+                        <Avatar sx={{height: 50, width: 50}} alt={bids[0].firstName + " " + bids[0].lastName} src={"http://localhost:4941/api/v1/users/" + bids[0].bidderId + "/image"}/>
+                    </Grid>
+                    <Grid item sm={2.5} style={styles.center} sx={{justifyContent: 'center'}}>
+                        <Typography variant="h6"><Link to={"users/" + bids[0].bidderId}>{bids[0].firstName} {bids[0].lastName}</Link></Typography>
+                    </Grid>
+                    <Grid item sm={12} style={{textAlign: 'center'}}>
+                        <Typography variant="h6">Reserve: ${auction.reserve}</Typography>
+                    </Grid>
+                </Grid>
+            )
+        }
+    }
+    interface HeadCell {
+        id: string;
+        label: string;
+        numeric: boolean;
+    }
+    const bidHeaders: readonly HeadCell[] = [
+        { id: 'amount', label: 'Amount', numeric: true },
+        { id: 'time', label: 'Placed', numeric: false },
+        { id: 'bidder', label: 'Bidder', numeric: false },
+    ];
+    const bidsTable = () => {
+        return (
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            {bidHeaders.map((headCell) => (
+                                <TableCell
+                                    key={headCell.id}
+                                    align={'center'}
+                                    padding={'normal'}>
+                                    {headCell.label}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {bidRows()}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        )
+    }
+    const bidRows = () => {
+        return bids.map((bid: bid) =>
+            <TableRow hover
+                      tabIndex={-1}
+                      key={bid.bidderId}>
+                <TableCell align={'center'}>
+                    ${bid.amount}
+                </TableCell>
+                <TableCell align={'center'}>
+                    {Date(bid.timestamp)}
+                </TableCell>
+                <TableCell align={'center'}>
+                    <Grid container spacing={1} style={{justifyContent: "center"}}>
+                    <Grid item sm={6} style={styles.center} sx={{justifyContent: 'center'}}>
+                        {bid.firstName} {bid.lastName}
+                        &nbsp;&nbsp;&nbsp;
+                        <Avatar sx={{height: 35, width: 35}} alt={bid.firstName + " " + bid.lastName} src={"http://localhost:4941/api/v1/users/" + bid.bidderId + "/image"}/>
+                    </Grid>
+
+                </Grid>
+                </TableCell>
+            </TableRow>
+        )
     }
     return (
         <div>
@@ -72,7 +215,16 @@ const Auction = () => {
                                 </Grid>
                             </Grid>
                         </div>
-
+                            <Divider />
+                        <div>
+                            <br></br>
+                            {auctionReserve()}
+                            <br></br>
+                            <Divider />
+                        </div>
+                            <div>
+                                    {bidsTable()}
+                            </div>
                         </Paper>
                     </List>
 
